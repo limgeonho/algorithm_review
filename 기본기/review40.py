76047f0636da8153e4e19e5208a8dbcf4c975352
limgeonho <ghlim909@gmail.com>
2021-12-03 17:18:52 +0900

    2021.12.03

---
+++ 기본기/review40.py
@@ -0,0 +1,80 @@
+n = int(input())
+array = list(map(int, input().split()))
+res = [0] * n
+visited = [False] * len(array)
+
+def perm(L):
+    if L == n:
+        print(*res)
+        return
+    for i in range(len(array)):
+        if visited[i]:
+            continue
+        else:
+            visited[i] = True
+            res[L] = array[i]
+            perm(L+1)
+            visited[i] = False
+
+perm(0)
+print('==================')
+
+def comb(L, start):
+    if L == n:
+        print(*res)
+        return
+    for i in range(start, len(array)):
+        res[L] = array[i]
+        comb(L+1, i+1)
+
+comb(0, 0)
+print('==================')
+
+
+def perm_with(L):
+    if L == n:
+        print(*res)
+        return
+    for i in range(len(array)):
+        res[L] = array[i]
+        perm_with(L+1)
+
+perm_with(0)
+print('==================')
+
+
+def subset(L, ss):
+    if L == n:
+        print(ss)
+        return
+    subset(L+1, ss + [array[L]])
+    subset(L+1, ss)
+
+subset(0, [])
+
+
+def next_perm(array):
+    i = len(array) - 1
+
+    while i > 0 and array[i-1] >= array[i]:
+        i -= 1
+    if i <= 0:
+        return False
+
+    j = len(array) - 1
+    while array[i-1] >= array[j]:
+        j -= 1
+    array[i-1], array[j] = array[j], array[i-1]
+
+    j = len(array) - 1
+
+    while i < j:
+        array[i], array[j] = array[j], array[i]
+        i += 1
+        j -= 1
+    return True
+
+if next_perm(array):
+    print(array)
+else:
+    print(-1)
