9cd176faf45a022c52a3ffed502caed032ca6044
limgeonho <ghlim909@gmail.com>
2021-12-08 00:07:17 +0900

    2021.12.08

--- 기본기/review43.py
+++ 기본기/review43.py
@@ -328,3 +328,17 @@
 # else:
 #     print(-1)
 ##############################################################
+# n = 1000
+# array = [True for i in range(n+1)]
+#
+# for i in range(2, int(n**0.5)+1):
+#     if array[i] == True:
+#         j = 2
+#         while i * j <= n:
+#             array[i * j] = False
+#             j += 1
+#
+# for i in range(2, n+1):
+#     if array[i]:
+#         print(i, end=' ')
+##############################################################
