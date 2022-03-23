3649bc90531453e4f7665119075ddc496c21480d
limgeonho <ghlim909@gmail.com>
2022-03-23 14:40:23 +0900

    inflearn

---
+++ inflearn/DP/최대 부분 증가수열.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+arr = list(map(int, input().split()))
+dp = [1] * len(arr)
+
+for i in range(1, n):
+    for j in range(i):
+        if arr[i] > arr[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+order = max(dp)
+answer = []
+
+for i in range(n-1, -1, -1):
+    if dp[i] == order:
+        answer.append(arr[i])
+        order -= 1
+# print(answer[::-1])
+# print(dp)
+print(max(dp))
