093438745d172ddf29957d03a1a796df1f5bbbfc
limgeonho <ghlim909@gmail.com>
2022-05-05 23:56:33 +0900

    lv3

---
+++ programmers/Lv3/거스름돈.py
@@ -0,0 +1,10 @@
+def solution(n, money):
+    answer = 0
+    dp = [1] + [0] * n
+    
+    for coin in money:
+        for price in range(coin, n+1):
+            if price >= coin:
+                dp[price] += dp[price - coin]
+    
+    return dp[n] % 1000000007
